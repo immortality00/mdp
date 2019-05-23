23c496fad4b0b316e873190656644e195433848f
Hussain Radhi <immortality00@Hussains-MacBook-Pro.local>
2019-05-23 17:48:49 +0400

    updating

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,37 @@
+'use strict';
 import Head from "next/head";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import React, {Component} from 'react';
 
+class Index extends Component {
+  constructor (props) {
+    super(props);
 
-const Index = () => (
-  
+    this.state = {
+        videoURL: '/static/MyMovie.mov'
+    }
+}
+
+render () {
+    return (
+
   <div id="wrapper">
-    <Head>
-      <title>Magic Point Decoration LLC</title>
-      <link href="/static/css/main.css" rel="stylesheet" />
-      <link href="/static/css/noscript.css" rel="stylesheet" />
-    </Head>
-    <Header />
-    <Footer />
-    <video loop  autoPlay preload="none" className="video" ><source src="./static/MyMovie.mov" type="video/mp4" /></video>
-
-  </div>
-   
-);
+  <Head>
+    <title>Magic Point Decoration LLC</title>
+    <link href="/static/css/main.css" rel="stylesheet" />
+  </Head>
+  <video className="video" loop autoPlay>
+            <source src={this.state.videoURL} type="video/mp4" />
+            <source src={this.state.videoURL} type="video/ogg" />
+            Your browser does not support the video tag.
+        </video>
+  <Header />
+  <Footer />
+</div>
+        
+    )
+}
+};
 
 export default Index;
